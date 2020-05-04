62b60a243c1080f382965842e40150973e39e257
Takafumi Suzuki <highdro0479@gmail.com>
2020-05-05 03:01:11 +0900

    add: empty animation

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tweet_separator/utils/twitter_client.dart';
@@ -23,8 +24,13 @@ class _TweetList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final viewModel = Provider.of<HomeViewModel>(context);
-    return !viewModel.isPrepared
-        ? const Center(child: CircularProgressIndicator())
+
+    if (!viewModel.isPrepared) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return viewModel.recentTweets.length == 0
+        ? _buildEmpty()
         : _buildTweets(viewModel);
   }
 
@@ -46,6 +52,48 @@ class _TweetList extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+
+  Widget _buildEmpty() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Spacer(),
+        Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 240,
+                height: 300,
+                child: const DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 300,
+              ),
+              child: const FlareActor(
+                'assets/mr_trash.flr',
+                animation: 'open_close',
+                fit: BoxFit.contain,
+              ),
+            ),
+          ],
+        ),
+        const Spacer(),
+        const Text('今日の分の断捨離は\n終わりました。'),
+        const Text('一日を楽しみましょう！'),
+        const Spacer(
+          flex: 3,
+        )
+      ],
     );
   }
 }
